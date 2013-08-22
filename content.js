e4402f2dc8c3d35605b8543cec08fe8250108439
kageroh <masao.yokoyama@gmail.com>
2013-08-22 17:48:51 +0900

    疲労度の差分を括弧書きで表示するようにした

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ var div = document.createElement('div');
 div.style.whiteSpace = 'pre';
 div.style.position = 'absolute';
 div.style.top = '7em';
-div.style.right = '1em';
+div.style.right = '.5em';
 document.body.appendChild(div);
 chrome.runtime.onMessage.addListener(function (req) {
 	div.textContent = req.join('\n');
--- devtools.js
+++ devtools.js
@@ -1,3 +1,5 @@
+var $ship_list = localStorage['ship_list']
+$ship_list = ($ship_list) ? JSON.parse($ship_list) : [];
 chrome.devtools.network.onRequestFinished.addListener(function (request) {
 	if (!/^http:\/\/[^\/]+\/kcsapi\/api_get_member\/ship2$/.test(request.request.url)) return;
 	var date = new Date();
@@ -11,16 +13,26 @@ chrome.devtools.network.onRequestFinished.addListener(function (request) {
 		var json = JSON.parse(content.replace(/^[^=]+=/, ''));
 		var data_list = json.api_data;
 		var deck_list = json.api_data_deck;
+
+		for (var i = 0, data; data = data_list[i]; i++) {
+			var ship = $ship_list[data.api_id];
+			$ship_list[data.api_id] = {
+				p_cond: (ship) ? ship.c_cond : 49,
+				c_cond: data.api_cond
+			};
+		}
+		localStorage['ship_list'] = JSON.stringify($ship_list);
+
 		for (var i = 0, deck; deck = deck_list[i]; i++) {
 			req.push(deck.api_name);
-			var ship_list = deck.api_ship;
-			for (var j = 0, ship; ship = ship_list[j]; j++) {
-				for (var k = 0, data; data = data_list[k]; k++) {
-					if (data.api_id === ship) {
-						req.push((j + 1).toString(10) + ': ' + data.api_cond.toString(10));
-						break;
-					}
-				}
+			var id_list = deck.api_ship;
+			for (var j = 0, id; id = id_list[j]; j++) {
+				if (id === -1) break;
+				var ship = $ship_list[id];
+				var cond = ship.c_cond;
+				var diff = cond - ship.p_cond;
+				diff = ((diff > 0) ? '+' : '') + diff.toString(10); 
+				req.push((j + 1).toString(10) + ': ' + cond.toString(10) + ' (' + diff + ')');
 			}
 		}
 		chrome.extension.sendRequest(req);
