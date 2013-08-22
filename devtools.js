4edc9264a2d1ffa1aa66c370496aa2c9c427a261
kageroh <masao.yokoyama@gmail.com>
2013-08-22 21:45:15 +0900

    $ship_listは配列だとnullが多すぎてlocalStorageを消費するのでハッシュにした

--- devtools.js
+++ devtools.js
@@ -1,5 +1,6 @@
 var $ship_list = localStorage['ship_list']
-$ship_list = ($ship_list) ? JSON.parse($ship_list) : [];
+$ship_list = ($ship_list) ? JSON.parse($ship_list) : {};
+
 chrome.devtools.network.onRequestFinished.addListener(function (request) {
 	if (!/^http:\/\/[^\/]+\/kcsapi\/api_get_member\/ship2$/.test(request.request.url)) return;
 	var date = new Date();
@@ -16,7 +17,7 @@ chrome.devtools.network.onRequestFinished.addListener(function (request) {
 
 		for (var i = 0, data; data = data_list[i]; i++) {
 			var ship = $ship_list[data.api_id];
-			$ship_list[data.api_id] = {
+			$ship_list[data.api_id.toString(10)] = {
 				p_cond: (ship) ? ship.c_cond : 49,
 				c_cond: data.api_cond
 			};
@@ -28,7 +29,7 @@ chrome.devtools.network.onRequestFinished.addListener(function (request) {
 			var id_list = deck.api_ship;
 			for (var j = 0, id; id = id_list[j]; j++) {
 				if (id === -1) break;
-				var ship = $ship_list[id];
+				var ship = $ship_list[id.toString(10)];
 				var cond = ship.c_cond;
 				var diff = cond - ship.p_cond;
 				diff = ((diff > 0) ? '+' : '') + diff.toString(10); 
