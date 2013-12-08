76e42c9e28656e294600827c97be9d929d168c20
t-f-m <ohmizaiju@s33.xrea.com>
2013-12-09 02:16:48 +0900

    nextやstartで得られる取得itemの表示

--- content.js
+++ content.js
@@ -8,6 +8,12 @@ document.body.appendChild(div);
 chrome.runtime.onMessage.addListener(function (req) {
 	if (req instanceof Array) {
 		div.textContent = req.join('\n');
+	} else if (typeof req === 'object') {
+		var msg = '\n\n';
+		for(key in req){
+			msg += key + req[key].toString(10) + '\n';
+		}
+		div.textContent += msg;
 	} else {
 		div.textContent += '\n\nnext enemy\n' + req.toString(10);
 	}
