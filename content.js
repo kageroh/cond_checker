16b7515a0244b3bda55ee1f4b241e10d8cdc03a0
hkuno9000 <hkuno9000+git@gmail.com>
2014-08-31 18:35:21 +0900

    リファクタリング: markdown出力のスタイル指定を改善した
    style直接指定をやめる
    スタイルクラスを登録してclass指定で参照するようにした

--- content.js
+++ content.js
@@ -6,6 +6,13 @@ div.style.left = '50%';
 div.style.marginLeft = '402px';
 document.body.appendChild(div);
 
+var style = document.createElement('style');
+style.textContent = "ul.markdown {list-style:disc inside;}" // 箇条書き頭文字円盤.
+	+ "table.markdown {border-collapse:collapse; border:0px;}" // テーブル枠線なし.
+	+ "table.markdown tr td {padding:0px 0.5em;}" // table cellpadding 上下0px, 左右0.5文字.
+	;
+document.getElementsByTagName('head')[0].appendChild(style);
+
 chrome.runtime.onMessage.addListener(function (req) {
 	if (req instanceof Array) {
 		div.innerHTML = parse_markdown(req);
@@ -35,12 +42,12 @@ function parse_markdown(a) {
 		else if (/^## /.test(s))	t = s.replace(/^#+ (.+)/, "<h3>$1</h3>");
 		else if (/^# /.test(s))		t = s.replace(/^#+ (.+)/, "<h2>$1</h2>");
 		else if (/^\* /.test(s))	{ t = s.replace(/^. (.+)/, "<li>$1</li>"); li_count++; }
-		else if (/\t/.test(s))		{ t = "<tr><td>" + s.replace(/\t/, "<td>") + "</tr>"; tr_count++; }
+		else if (/^\t/.test(s))		{ t = "<tr>" + s.replace(/\t/g, "<td>") + "</tr>"; tr_count++; }
 		// リストを<ul>で括る.
-		if (li_count == 1) html += "<ul style=\"list-style:disc inside;\">";
+		if (li_count == 1) html += '<ul class="markdown">';
 		if (li_count > 0 && !/^<li>/.test(t)) { li_count = 0; html += "</ul>"; } 
 		// テーブルを<table>で括る.
-		if (tr_count == 1) html += "<table cellspacing=0 cellpadding=0>";	// html4.0の古い指定方法なのでCSSにしたい.
+		if (tr_count == 1) html += '<table class="markdown">';
 		if (tr_count > 0 && !/^<tr>/.test(t)) { tr_count = 0; html += "</table>"; } 
 		// 変換結果をhtmlに格納する.
 		if (t) html += t;
