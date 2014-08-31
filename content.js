b4b90aa165d150964f01ca91a85fb75522216597
hkuno9000 <hkuno9000+git@gmail.com>
2014-08-31 15:22:18 +0900

    リファクタリング: markdownパーサを書き直した

--- content.js
+++ content.js
@@ -8,30 +8,39 @@ document.body.appendChild(div);
 
 chrome.runtime.onMessage.addListener(function (req) {
 	if (req instanceof Array) {
-		div.innerHTML = parse_markdown(req.join('\n'));
+		div.innerHTML = parse_markdown(req);
 	} else {
-		div.innerHTML += parse_markdown(req.toString());
+		div.innerHTML += parse_markdown(req.toString().split('\n'));
 	}
 });
 
-function parse_markdown(s) {
-	// エスケープを行う.
-	s = s.replace(/\&/g, "&amp;");
-	s = s.replace(/\</g, "&lt;");
-	s = s.replace(/\>/g, "&gt;");
-	// markdown書式をhtmlに変換する.
-	s = s.replace(/^([#\*\-])/, "\n$1"); // 行頭がmarkdown書式なら、センチネルとして改行を付ける.
-	s = s.replace(/\n#### ([^\n]+)/g, "<h5>$1</h5>");
-	s = s.replace(/\n### ([^\n]+)/g, "<h4>$1</h4>");
-	s = s.replace(/\n## ([^\n]+)/g, "<h3>$1</h3>");
-	s = s.replace(/\n# ([^\n]+)/g, "<h2>$1</h2>");
-	s = s.replace(/\n\* ([^\n]+)/g, "<li>$1</li>"); // <ul>で括るのが正しいが、手抜きする.
-	s = s.replace(/\n--+/g, "<hr>");
-	// 余分な改行を削除する.
-	s = s.replace(/>\n+/g, ">");
-	s = s.replace(/\n+</g, "<");
-	// 色付け.
-	s = s.replace(/撃沈---/g, '<span style="color:steelblue">$&</span>');
-	s = s.replace(/大破!!!/g, '<span style="color:red">$&</span>');
-	return s;
+function parse_markdown(a) {
+	var html = "";
+	var li_count = 0;
+	for (var i = 0; i < a.length; ++i) {
+		var s = a[i];
+		var t = null;
+		// エスケープを行う.
+		s = s.replace(/\&/g, "&amp;");
+		s = s.replace(/\</g, "&lt;");
+		s = s.replace(/\>/g, "&gt;");
+		// 色付け.
+		s = s.replace(/撃沈---/g, '<span style="color:steelblue">$&</span>');
+		s = s.replace(/大破!!!/g, '<span style="color:red">$&</span>');
+		// markdown書式を変換する.
+		if      (/^--+/.test(s))	t = "<hr>";
+		else if (/^#### /.test(s))	t = s.replace(/^#+ (.+)/, "<h5>$1</h5>");
+		else if (/^### /.test(s))	t = s.replace(/^#+ (.+)/, "<h4>$1</h4>");
+		else if (/^## /.test(s))	t = s.replace(/^#+ (.+)/, "<h3>$1</h3>");
+		else if (/^# /.test(s))		t = s.replace(/^#+ (.+)/, "<h2>$1</h2>");
+		else if (/^\* /.test(s))	{ t = s.replace(/^. (.+)/, "<li>$1</li>"); li_count++; }
+		// リストを<ul>で括る.
+		if (li_count == 1) html += "<ul>";
+		if (li_count > 0 && !/^<li>/.test(t)) { li_count = 0; html += "</ul>"; } 
+		// 変換結果をhtmlに格納する.
+		if (t) html += t;
+		else   html += s + "\n";
+	}
+	return html;
 }
+
