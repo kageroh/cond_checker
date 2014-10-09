bcf68a970a8dcc7a45de32483e649217b01503ef
kageroh <masao.yokoyama@gmail.com>
2014-10-10 03:45:46 +0900

    装備変更のたびにtimestampが変わって鬱陶しいからship3はとりあえず見ない

--- background.js
+++ background.js
@@ -5,7 +5,6 @@ chrome.webRequest.onCompleted.addListener(function (details) {
 }, {
 	urls: [
 		"http://*/kcsapi/api_get_member/ship2",
-		"http://*/kcsapi/api_get_member/ship3",
 		"http://*/kcsapi/api_port/port"
 	],
 	types: ["other"]
