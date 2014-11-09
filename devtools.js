7abfef97bf48050ca9b6ef16f2ee7f501b139691
t-f-m <ohmizaiju@s33.xrea.com>
2014-11-10 06:59:37 +0900

    B勝利・C敗北の条件を修正
    B勝利の条件を戦果ゲージ2.5倍以上、C敗北の条件を戦果ゲージ等倍以上に修正
    また戦果ゲージの基準を戦闘開始時のHPに変更した

--- devtools.js
+++ devtools.js
@@ -780,11 +780,11 @@ function push_guess_result(req, nowhps, maxhps, beginhps) {
 		var n = nowhps[i];
 		if(i <= 6){
 			f_damage_total += beginhps[i] - Math.max(0, n);
-			f_hp_total += maxhps[i];
+			f_hp_total += beginhps[i];
 		} else if(i > 6){
 			++e_count;
 			e_damage_total += beginhps[i] - Math.max(0, n);
-			e_hp_total += maxhps[i];
+			e_hp_total += beginhps[i];
 			if(n <= 0){
 				++e_lost_count;
 				if(i == 7) e_leader_lost = true;
@@ -806,18 +806,20 @@ function push_guess_result(req, nowhps, maxhps, beginhps) {
 	var f_damage_rate = f_damage_total/f_hp_total;
 	var e_damage_rate = e_damage_total/e_hp_total;
 	var rate = e_damage_rate == 0 ? 0 : // 潜水艦お見合い等ではDになるので敵ダメ判定を優先
-			   f_damage_rate == 0 ? 2 : // 0除算回避／こちらが無傷なら1ダメ以上与えていればBなのでrateを2に
+			   f_damage_rate == 0 ? 3 : // 0除算回避／こちらが無傷なら1ダメ以上与えていればBなのでrateを3に
 			   e_damage_rate/f_damage_rate;
-	if(e_leader_lost || rate >= 2){
+	if(e_leader_lost || rate >= 2.5){
 		req.push('推定：B');
 		return;
-	} else if(rate >= 1/2){ //要検証
+	} else if(rate >= 1){ //要検証
 		req.push('推定：C');
 		return;
 	} else {
 		req.push('推定：D');
 		return;
 	}
+	req.push('推定：E');
+	return;
 }
 
 function on_battle(json) {
