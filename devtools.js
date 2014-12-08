5be8a7aa16b954519c6abcb0e951700f0afa04db
hkuno9000 <hkuno9000+git@gmail.com>
2014-12-09 03:46:07 +0900

    勝敗推定が間違っていたとき警告とデバッグ情報を表示する

--- devtools.js
+++ devtools.js
@@ -31,7 +31,7 @@ var $beginhps_c = null;
 var $f_damage_rate = 0;
 var $e_damage_rate = 0;
 var $guess_win_rank = '?';
-
+var $guess_info_str = '';
 
 //-------------------------------------------------------------------------
 // Ship クラス.
@@ -259,6 +259,10 @@ function percent_name(now, max) {	// now:1, prev:2 -> "50%"
 function percent_name_unless100(now, max) {	// now:1, max:2 -> "(50%)"
 	if (!max || now == max) return '';
 	return '(' + percent_name(now, max) + ')';
+}
+
+function fraction_percent_name(now, max) {	// now:1, max:2 -> "1/2(50%)"
+	return now + '/' + max + '(' + percent_name(now, max) + ')';
 }
 
 function kira_name(cond) {
@@ -947,6 +951,9 @@ function on_battle_result(json) {
 			if (rank == 'S' && $f_damage_rate == 0) rank = '完S';
 		}
 		msg += ':' + rank;
+		if (rank != $guess_win_rank) {
+			msg += '\n### @!!勝敗推定ミス!!@ ' + $guess_info_str;
+		}
 		var fleet = $enemy_list[$enemy_id];
 		if (fleet) {
 			fleet[0] = e.api_deck_name + '(' + formation_name($enemy_formation_id) + '):';
@@ -1098,6 +1105,7 @@ function guess_win_rank(nowhps, maxhps, beginhps, nowhps_c, maxhps_c, beginhps_c
 	}
 	$f_damage_rate = f_damage_total / f_hp_total;
 	$e_damage_rate = e_damage_total / e_hp_total;
+	$guess_info_str = 'f_damage:' + fraction_percent_name(f_damage_total, f_hp_total) + ', e_damage:' + fraction_percent_name(e_damage_total, e_hp_total);
 	if (e_count == e_lost_count && f_lost_count == 0) {
 		return (f_damage_total == 0) ? '完S' : 'S';
 	}
