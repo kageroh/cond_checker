2cecdd612639195ce13c3e5f1c4356234e8ee65d
hkuno9000 <hkuno9000+git@gmail.com>
2015-03-07 17:32:19 +0900

    [DEBUG] #24 勝敗判定ミス調査
    戦闘結果B/C/Dについて、判定情報をログ記録する

--- devtools.js
+++ devtools.js
@@ -1012,6 +1012,8 @@ function on_battle_result(json) {
 		}
 		$battle_log.push($next_enemy + '(' + e.api_deck_name + '):' + rank);
 		$last_mission[$battle_deck_id] = '前回出撃: ' + $battle_log.join(' →');
+		if (rank == 'B' || rank == 'C' || rank == 'D') ///@DEBUG check B/C/D
+			push_to_logbook($next_enemy + ', ' + rank + '/' + $guess_win_rank + ', ' + $guess_info_str);
 	}
 	if (mvp) {
 		var id = $fdeck_list[$battle_deck_id].api_ship[mvp-1];
@@ -1262,6 +1264,7 @@ function on_battle(json) {
 		});
 	}
 	$guess_win_rank = guess_win_rank(nowhps, maxhps, $beginhps, nowhps_c, maxhps_c, $beginhps_c);
+	req.push($guess_info_str); ///@DEBUG
 	req.push('勝敗推定:' + $guess_win_rank);
 
 	req.push('## friend damage');
